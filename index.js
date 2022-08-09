62fb4fd7448b427c995279ed14e3045cbd0d4472
Eduardo Silva <eduardo.luiz.s@hotmail.com>
2022-08-09 14:21:30 -0300

    port

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const path = require("path");
-const apiRoute = require('./routes/api');
+const apiRoute = require("./routes/api");
 
-
-app.use('/api', apiRoute);
-app.use('/', express.static(path.join(__dirname, "public")));
-
+app.use("/api", apiRoute);
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-    console.log("Server running in port", PORT);
+  console.log("Server running in port", PORT);
 });
-
